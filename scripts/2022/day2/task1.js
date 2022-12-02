9cf6bcc60f455fe4b54008091160a3f81cc30b64
Killerbear <killerbear_02@gmx.de>
2022-12-02 16:14:25 +0100

    2022 day 2

---
+++ scripts/2022/day2/task1.js
@@ -0,0 +1,22 @@
+const Combinations1 = new Map();
+
+function solve2022Day2Task1(inputString) {
+    setAllPossibleCombination1();
+    let solution = 0;
+    let games = inputString.split("\n");
+
+    games.forEach(game => solution += Combinations1.get(game))
+    return solution;
+}
+
+function setAllPossibleCombination1() {
+    Combinations1.set("A X", 4);
+    Combinations1.set("A Y", 8);
+    Combinations1.set("A Z", 3);
+    Combinations1.set("B X", 1);
+    Combinations1.set("B Y", 5);
+    Combinations1.set("B Z", 9);
+    Combinations1.set("C X", 7);
+    Combinations1.set("C Y", 2);
+    Combinations1.set("C Z", 6);
+}
---
+++ scripts/2022/day2/task2.js
@@ -0,0 +1,22 @@
+const Combinations2 = new Map();
+
+function solve2022Day2Task2(inputString) {
+    setAllPossibleCombination2();
+    let solution = 0;
+    let games = inputString.split("\n");
+
+    games.forEach(game => solution += Combinations2.get(game))
+    return solution;
+}
+
+function setAllPossibleCombination2() {
+    Combinations2.set("A X", 3);
+    Combinations2.set("A Y", 4);
+    Combinations2.set("A Z", 8);
+    Combinations2.set("B X", 1);
+    Combinations2.set("B Y", 5);
+    Combinations2.set("B Z", 9);
+    Combinations2.set("C X", 2);
+    Combinations2.set("C Y", 6);
+    Combinations2.set("C Z", 7);
+}
