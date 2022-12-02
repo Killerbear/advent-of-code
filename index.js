be4a2214ab12bea81794de429b39741304964015
Frank Springsguth <f.springsguth@itsonix.eu>
2022-12-02 20:42:39 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 let input = document.getElementById('input');
 let output = document.getElementById('output');
-
 const buttons = document.getElementsByTagName("button");
+let scripts = [];
+
+input.value = localStorage.getItem('puzzleInput');
+
 for (let button of buttons) {
-    button.onclick = () => output.value = window[button.id](input.value)
+    button.onclick = () => {
+        let src = generateSrc(button.id);
+        addScriptAndStartCalculation(src,button.id)
+    }
+}
+
+function generateSrc(id) {
+    let year = id.slice(5,9);
+    let day = id.slice(12,13);
+    let task = id.slice(17,18);
+    return `./scripts/${year}/day${day}/task${task}.js`
+}
+
+function addScriptAndStartCalculation(src,buttonId) {
+    if (!scripts.includes(src)) {
+        scripts.push(src);
+        const script = document.createElement("script");
+        script.src = src;
+        document.body.appendChild(script);
+        script.onload = function() {
+            calculateOutput(buttonId);
+        }
+    } else {
+        calculateOutput(buttonId);
+    }
+}
+
+function calculateOutput(id) {
+    output.value = window[id](input.value);
+    localStorage.setItem('puzzleInput', input.value);
 }
