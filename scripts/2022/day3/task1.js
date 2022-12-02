477b6083c38f401981ad6f9ef406cb45978391bd
Frank Springsguth <f.springsguth@itsonix.eu>
2022-12-02 19:13:11 +0100

    2022 day 3

---
+++ scripts/2022/day3/task1.js
@@ -0,0 +1,4 @@
+function solve2022Day3Task1(inputString) {
+    let solution = 0;
+    return solution;
+}
---
+++ scripts/2022/day3/task2.js
@@ -0,0 +1,4 @@
+function solve2022Day3Task2(inputString) {
+    let solution = 0;
+    return solution;
+}
