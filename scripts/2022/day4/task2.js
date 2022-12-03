4448aecdf0f48439bf2d54df4d83a894ce7c1555
Frank Springsguth <f.springsguth@itsonix.eu>
2022-12-03 11:19:44 +0100

    2022 day 4

---
+++ scripts/2022/day4/task2.js
@@ -0,0 +1,15 @@
+function solve2022Day4Task2(inputString) {
+	let solution = 0;
+	let assignmentPairs = inputString.split("\n");
+	let assignments = [];
+	assignmentPairs.forEach((assignmentPair, index) => (assignments[index] = assignmentPair.split(",")));
+	assignments = assignments.flat();
+	for (let i = 0; i < assignments.length; i = i + 2) {
+		let [min1, max1] = assignments[i].split("-").map((item) => parseInt(item, 10));
+		let [min2, max2] = assignments[i + 1].split("-").map((item) => parseInt(item, 10));
+		if ((max1 >= min2 && min1 <= min2) || (max2 >= min1 && min2 <= min1)) {
+			solution++;
+		}
+	}
+	return solution;
+}
