47cb7e3a0f5b4af2035ba840a0cc18f36b32ce9d
Killerbear <killerbear_02@gmx.de>
2023-12-02 12:23:06 +0100

    2023 day 2

--- scripts/2023/day02/task1.js
+++ scripts/2023/day02/task1.js
@@ -1,1 +1,31 @@
-function solve2023Day02Task1(inputString) {}
+function solve2023Day02Task1(inputString) {
+    const givenMax = new Map();
+    givenMax.set("red", 12);
+    givenMax.set("green", 13);
+    givenMax.set("blue", 14);
+    let validGamesIdSum = 0;
+    let games = inputString.split("\n");
+
+    games.forEach((gameString) => {
+        let isValidGame = true;
+
+        let [id, game] = gameString.split(":");
+        id = id.slice(5);
+
+        game.split(";")
+            .map((set) => set.split(",").map((value) => value.trim()))
+            .flat()
+            .map((cubes) => {
+                const [number, color] = cubes.split(" ");
+                if (Number(number) > givenMax.get(color)) {
+                    isValidGame = false;
+                }
+            });
+
+        if (isValidGame) {
+            validGamesIdSum += Number(id);
+        }
+    });
+
+    return validGamesIdSum;
+}
--- scripts/2023/day02/task2.js
+++ scripts/2023/day02/task2.js
@@ -1,1 +1,31 @@
-function solve2023Day02Task2(inputString) {}
+function solve2023Day02Task2(inputString) {
+    let powersSum = 0;
+    let games = inputString.split("\n");
+
+    games.forEach((gameString) => {
+        let [, game] = gameString.split(":");
+        let max = initHashMap();
+
+        game.split(";")
+            .map((set) => set.split(",").map((value) => value.trim()))
+            .flat()
+            .map((cubes) => {
+                const [number, color] = cubes.split(" ");
+                if (Number(number) > max.get(color)) {
+                    max.set(color, number);
+                }
+            });
+
+        powersSum += max.get("red") * max.get("green") * max.get("blue");
+    });
+
+    return powersSum;
+}
+
+function initHashMap() {
+    const map = new Map();
+    map.set("red", 0);
+    map.set("green", 0);
+    map.set("blue", 0);
+    return map;
+}
