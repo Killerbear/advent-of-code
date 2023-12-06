bcef0032a1f0fe83b20b1633b70d4247b446ae4d
Killerbear <f.springsguth@itsonix.eu>
2023-12-06 20:53:55 +0100

    2023 day 6

--- scripts/2023/day06/task1.js
+++ scripts/2023/day06/task1.js
@@ -1,1 +1,34 @@
-function solve2023Day06Task1(inputString) {}
+function solve2023Day06Task1(inputString) {
+    const stringLines = inputString.split("\n");
+    const times = stringLines[0]
+        .split(":")[1]
+        .split(" ")
+        .filter(Boolean)
+        .map((number) => Number(number));
+
+    const distances = stringLines[1]
+        .split(":")[1]
+        .split(" ")
+        .filter(Boolean)
+        .map((number) => Number(number));
+
+    const winningRacesCount = [];
+    for (let i = 0; i < times.length; i++) {
+        winningRacesCount.push(0);
+    }
+
+    times.forEach((time, index) => {
+        console.log("Race " + index);
+        for (let i = 1; i < time; i++) {
+            const raceDistance = i * (time - i);
+            console.log(
+                `holdTime: ${i}, raceTime: ${time - i}, raceDistance: ${raceDistance}, maxDistance: ${distances[index]}`
+            );
+            if (raceDistance > distances[index]) {
+                winningRacesCount[index]++;
+            }
+        }
+    });
+
+    return winningRacesCount.reduce((sum, count) => sum * count);
+}
--- scripts/2023/day06/task2.js
+++ scripts/2023/day06/task2.js
@@ -1,1 +1,23 @@
-function solve2023Day06Task2(inputString) {}
+function solve2023Day06Task2(inputString) {
+    const stringLines = inputString.split("\n");
+    const time = Number(stringLines[0].split(":")[1].split(" ").filter(Boolean).join(""));
+    const distance = Number(stringLines[1].split(":")[1].split(" ").filter(Boolean).join(""));
+
+    const min = 1;
+
+    let isFinished = false;
+    let index = min;
+    let lostRaces = 0;
+
+    while (!isFinished) {
+        const raceDistance = index * (time - index);
+        if (raceDistance > distance) {
+            isFinished = !isFinished;
+        } else {
+            lostRaces++;
+        }
+        index++;
+    }
+
+    return time - lostRaces * 2 - 1;
+}
