5b39f7497e3a434d9a7fb017a005da63f064844a
Killerbear <killerbear_02@gmx.de>
2023-12-09 12:48:02 +0100

    2023 day 9

--- scripts/2023/day09/task1.js
+++ scripts/2023/day09/task1.js
@@ -1,1 +1,51 @@
-function solve2023Day09Task1(inputString) {}
+let allSequences = [];
+
+function solve2023Day09Task1(inputString) {
+    allSequences = [];
+    const historyLines = inputString.split("\n");
+    let result = 0;
+
+    for (const element of historyLines) {
+        allSequences.push([]);
+    }
+
+    historyLines.forEach((history, index) => {
+        const historyArray = history.split(" ").map((number) => Number(number));
+        allSequences[index].push(historyArray);
+        calculateSequence(historyArray, index);
+    });
+
+    allSequences.forEach((historySequence) => {
+        if (historySequence.length === 2) {
+            result += historySequence[0].slice(-1);
+        } else {
+            for (let i = historySequence.length - 3; i >= 0; i--) {
+                const number = Number(historySequence[i].slice(-1)) + Number(historySequence[i + 1].slice(-1));
+                historySequence[i].push(number);
+                if (i === 0) {
+                    result += number;
+                }
+            }
+        }
+    });
+
+    return result;
+}
+
+function calculateSequence(sequence, index) {
+    let isLastSequence = true;
+    let newSequence = [];
+    for (let i = 0; i < sequence.length - 1; i++) {
+        const difference = sequence[i + 1] - sequence[i];
+        newSequence[i] = difference;
+        if (difference !== 0) {
+            isLastSequence = false;
+        }
+    }
+
+    allSequences[index].push(newSequence);
+
+    if (!isLastSequence) {
+        calculateSequence(newSequence, index);
+    }
+}
