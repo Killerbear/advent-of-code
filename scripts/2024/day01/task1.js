bba6a909afc267469dbff8f0ccb756c359e31140
Killerbear <killerbear_02@gmx.de>
2024-12-01 13:25:30 +0100

    2024 day 1

--- scripts/2024/day01/task1.js
+++ scripts/2024/day01/task1.js
@@ -1,1 +1,17 @@
-function solve2023Day01Task1(inputString) {}
+function solve2024Day01Task1(inputString) {
+    let result = 0;
+    let leftList = [];
+    let rightList = [];
+    inputString.split("\n").map((locationPair) => {
+        let locations = locationPair.split("   ");
+        leftList.push(parseInt(locations[0], 10));
+        rightList.push(parseInt(locations[1], 10));
+    });
+
+    leftList.sort();
+    rightList.sort();
+
+    leftList.forEach((location, index) => (result += Math.abs(location - rightList[index])));
+
+    return result;
+}
