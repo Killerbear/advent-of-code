127ab719534c1be01e3340cd3570f151369d6cdf
Killerbear <killerbear_02@gmx.de>
2024-12-02 23:18:41 +0100

    2024 day 2

--- scripts/2024/day02/task1.js
+++ scripts/2024/day02/task1.js
@@ -1,1 +1,34 @@
-function solve2024Day02Task1(inputString) {}
+function solve2024Day02Task1(inputString) {
+    let saveLevels = 0;
+    let inputLines = inputString.split("\n").map((report) => report.split(" "));
+    inputLines
+        .map((level) => level.map((value) => parseInt(value, 10)))
+        .forEach((level) => {
+            if (calculateSaveLevel(level)) {
+                saveLevels++;
+            }
+        });
+
+    return saveLevels;
+}
+
+function calculateSaveLevel(level) {
+    let increasing = true;
+    let decreasing = true;
+    let saveDistance = true;
+
+    level.reduce((accumulator, currentValue) => {
+        if (Math.abs(accumulator - currentValue) > 3 || Math.abs(accumulator - currentValue) === 0) {
+            saveDistance = false;
+        }
+        if (accumulator <= currentValue) {
+            decreasing = false;
+        }
+        if (accumulator >= currentValue) {
+            increasing = false;
+        }
+        return currentValue;
+    });
+
+    return (increasing || decreasing) && saveDistance;
+}
--- scripts/2024/day02/task2.js
+++ scripts/2024/day02/task2.js
@@ -1,1 +1,42 @@
-function solve2024Day02Task2(inputString) {}
+function solve2024Day02Task2(inputString) {
+    let saveLevels = 0;
+    let inputLines = inputString.split("\n").map((report) => report.split(" "));
+    inputLines
+        .map((level) => level.map((value) => parseInt(value, 10)))
+        .forEach((level) => {
+            let isSaveLevel = false;
+
+            for (let i = 0; i < level.length; i++) {
+                if (calculateSaveLevel([...level].filter((value, index) => index !== i))) {
+                    isSaveLevel = true;
+                }
+            }
+
+            if (isSaveLevel) {
+                saveLevels++;
+            }
+        });
+
+    return saveLevels;
+}
+
+function calculateSaveLevel(level) {
+    let increasing = true;
+    let decreasing = true;
+    let saveDistance = true;
+
+    level.reduce((accumulator, currentValue) => {
+        if (Math.abs(accumulator - currentValue) > 3 || Math.abs(accumulator - currentValue) === 0) {
+            saveDistance = false;
+        }
+        if (accumulator <= currentValue) {
+            decreasing = false;
+        }
+        if (accumulator >= currentValue) {
+            increasing = false;
+        }
+        return currentValue;
+    });
+
+    return (increasing || decreasing) && saveDistance;
+}
