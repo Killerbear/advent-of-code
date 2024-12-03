f85caf680290a00dceb197f663cbc53883620f7e
Killerbear <killerbear_02@gmx.de>
2024-12-03 22:20:18 +0100

    2024 day 3

--- scripts/2024/day03/task1.js
+++ scripts/2024/day03/task1.js
@@ -1,1 +1,12 @@
-function solve2024Day03Task1(inputString) {}
+function solve2024Day03Task1(inputString) {
+    const regex = /mul\(\d{1,3},\d{1,3}\)/g;
+    return inputString
+        .match(regex)
+        .map((mul) =>
+            mul
+                .match(/\d+/g)
+                .map((num) => parseInt(num))
+                .reduce((acc, cur) => acc * cur)
+        )
+        .reduce((acc, cur) => acc + cur);
+}
--- scripts/2024/day03/task2.js
+++ scripts/2024/day03/task2.js
@@ -1,1 +1,30 @@
-function solve2024Day03Task2(inputString) {}
+function solve2024Day03Task2(inputString) {
+    let removeElements = false;
+    const regex = /mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/g;
+    let matches = inputString.match(regex);
+    matches.forEach((match, index) => {
+        if (match === "do()") {
+            removeElements = false;
+            matches[index] = "##removed##";
+        }
+        if (removeElements) {
+            matches[index] = "##removed##";
+        }
+        if (match === "don't()") {
+            removeElements = true;
+            matches[index] = "##removed##";
+        }
+    });
+    return multiply(matches.filter((match) => match !== "##removed##"));
+}
+
+function multiply(array) {
+    return array
+        .map((mul) =>
+            mul
+                .match(/\d+/g)
+                .map((num) => parseInt(num))
+                .reduce((acc, cur) => acc * cur)
+        )
+        .reduce((acc, cur) => acc + cur);
+}
