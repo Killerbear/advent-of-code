88d68a02fb91fe11d3a9d23b17d68822405bc1ae
Killerbear <killerbear_02@gmx.de>
2024-12-07 10:42:11 +0100

    2024 day 4

--- scripts/2024/day04/task1.js
+++ scripts/2024/day04/task1.js
@@ -1,1 +1,126 @@
-function solve2024Day04Task1(inputString) {}
+function solve2024Day04Task1(inputString) {
+    let letter = inputString.split("\n").map((line) => line.split(""));
+    let xmasCounter = 0;
+
+    console.log("horizontal forward search");
+    for (let index_letter = 0; index_letter < letter.length; index_letter++) {
+        for (let index_char = 0; index_char < letter[index_letter].length - 3; index_char++) {
+            if (
+                letter[index_letter][index_char] === "X" &&
+                letter[index_letter][index_char + 1] === "M" &&
+                letter[index_letter][index_char + 2] === "A" &&
+                letter[index_letter][index_char + 3] === "S"
+            ) {
+                xmasCounter++;
+                console.log(index_letter, index_char);
+            }
+        }
+    }
+
+    console.log("horizontal backward search");
+    for (let index_letter = 0; index_letter < letter.length; index_letter++) {
+        for (let index_char = letter[index_letter].length - 1; index_char > 2; index_char--) {
+            if (
+                letter[index_letter][index_char] === "X" &&
+                letter[index_letter][index_char - 1] === "M" &&
+                letter[index_letter][index_char - 2] === "A" &&
+                letter[index_letter][index_char - 3] === "S"
+            ) {
+                xmasCounter++;
+                console.log(index_letter, index_char);
+            }
+        }
+    }
+
+    console.log("vertical forward search");
+    for (let index_letter = 0; index_letter < letter.length - 3; index_letter++) {
+        for (let index_char = 0; index_char < letter[index_letter].length; index_char++) {
+            if (
+                letter[index_letter][index_char] === "X" &&
+                letter[index_letter + 1][index_char] === "M" &&
+                letter[index_letter + 2][index_char] === "A" &&
+                letter[index_letter + 3][index_char] === "S"
+            ) {
+                xmasCounter++;
+                console.log(index_letter, index_char);
+            }
+        }
+    }
+
+    console.log("vertical backward search");
+    for (let index_letter = letter.length - 1; index_letter > 2; index_letter--) {
+        for (let index_char = 0; index_char < letter[index_letter].length; index_char++) {
+            if (
+                letter[index_letter][index_char] === "X" &&
+                letter[index_letter - 1][index_char] === "M" &&
+                letter[index_letter - 2][index_char] === "A" &&
+                letter[index_letter - 3][index_char] === "S"
+            ) {
+                xmasCounter++;
+                console.log(index_letter, index_char);
+            }
+        }
+    }
+
+    console.log("schräg runter rechts search");
+    for (let index_letter = 0; index_letter < letter.length - 3; index_letter++) {
+        for (let index_char = 0; index_char < letter[index_letter].length - 3; index_char++) {
+            if (
+                letter[index_letter][index_char] === "X" &&
+                letter[index_letter + 1][index_char + 1] === "M" &&
+                letter[index_letter + 2][index_char + 2] === "A" &&
+                letter[index_letter + 3][index_char + 3] === "S"
+            ) {
+                xmasCounter++;
+                console.log(index_letter, index_char);
+            }
+        }
+    }
+
+    console.log("schräg runter link search");
+    for (let index_letter = 0; index_letter < letter.length - 3; index_letter++) {
+        for (let index_char = letter[index_letter].length - 1; index_char > 2; index_char--) {
+            if (
+                letter[index_letter][index_char] === "X" &&
+                letter[index_letter + 1][index_char - 1] === "M" &&
+                letter[index_letter + 2][index_char - 2] === "A" &&
+                letter[index_letter + 3][index_char - 3] === "S"
+            ) {
+                xmasCounter++;
+                console.log(index_letter, index_char);
+            }
+        }
+    }
+
+    console.log("schräg hoch rechts search");
+    for (let index_letter = letter.length - 1; index_letter > 2; index_letter--) {
+        for (let index_char = 0; index_char < letter[index_letter].length - 3; index_char++) {
+            if (
+                letter[index_letter][index_char] === "X" &&
+                letter[index_letter - 1][index_char + 1] === "M" &&
+                letter[index_letter - 2][index_char + 2] === "A" &&
+                letter[index_letter - 3][index_char + 3] === "S"
+            ) {
+                xmasCounter++;
+                console.log(index_letter, index_char);
+            }
+        }
+    }
+
+    console.log("schräg hoch links search");
+    for (let index_letter = letter.length - 1; index_letter > 2; index_letter--) {
+        for (let index_char = letter[index_letter].length - 1; index_char > 2; index_char--) {
+            if (
+                letter[index_letter][index_char] === "X" &&
+                letter[index_letter - 1][index_char - 1] === "M" &&
+                letter[index_letter - 2][index_char - 2] === "A" &&
+                letter[index_letter - 3][index_char - 3] === "S"
+            ) {
+                xmasCounter++;
+                console.log(index_letter, index_char);
+            }
+        }
+    }
+
+    return xmasCounter;
+}
--- scripts/2024/day04/task2.js
+++ scripts/2024/day04/task2.js
@@ -1,1 +1,70 @@
-function solve2024Day04Task2(inputString) {}
+function solve2024Day04Task2(inputString) {
+    let letter = inputString.split("\n").map((line) => line.split(""));
+    let xmasCounter = 0;
+
+    console.log("-------------------");
+    for (let index_letter = 0; index_letter < letter.length - 2; index_letter++) {
+        for (let index_char = 0; index_char < letter[index_letter].length - 2; index_char++) {
+            if (
+                letter[index_letter][index_char] === "M" &&
+                letter[index_letter + 1][index_char + 1] === "A" &&
+                letter[index_letter + 2][index_char + 2] === "S" &&
+                letter[index_letter][index_char + 2] === "M" &&
+                letter[index_letter + 2][index_char] === "S"
+            ) {
+                xmasCounter++;
+                console.log(index_letter, index_char);
+            }
+        }
+    }
+
+    console.log("-------------------");
+    for (let index_letter = 0; index_letter < letter.length - 2; index_letter++) {
+        for (let index_char = 0; index_char < letter[index_letter].length - 2; index_char++) {
+            if (
+                letter[index_letter][index_char] === "M" &&
+                letter[index_letter + 1][index_char + 1] === "A" &&
+                letter[index_letter + 2][index_char + 2] === "S" &&
+                letter[index_letter][index_char + 2] === "S" &&
+                letter[index_letter + 2][index_char] === "M"
+            ) {
+                xmasCounter++;
+                console.log(index_letter, index_char);
+            }
+        }
+    }
+
+    console.log("-------------------");
+    for (let index_letter = 0; index_letter < letter.length - 2; index_letter++) {
+        for (let index_char = 0; index_char < letter[index_letter].length - 2; index_char++) {
+            if (
+                letter[index_letter][index_char] === "S" &&
+                letter[index_letter + 1][index_char + 1] === "A" &&
+                letter[index_letter + 2][index_char + 2] === "M" &&
+                letter[index_letter][index_char + 2] === "M" &&
+                letter[index_letter + 2][index_char] === "S"
+            ) {
+                xmasCounter++;
+                console.log(index_letter, index_char);
+            }
+        }
+    }
+
+    console.log("-------------------");
+    for (let index_letter = 0; index_letter < letter.length - 2; index_letter++) {
+        for (let index_char = 0; index_char < letter[index_letter].length - 2; index_char++) {
+            if (
+                letter[index_letter][index_char] === "S" &&
+                letter[index_letter + 1][index_char + 1] === "A" &&
+                letter[index_letter + 2][index_char + 2] === "M" &&
+                letter[index_letter][index_char + 2] === "S" &&
+                letter[index_letter + 2][index_char] === "M"
+            ) {
+                xmasCounter++;
+                console.log(index_letter, index_char);
+            }
+        }
+    }
+
+    return xmasCounter;
+}
